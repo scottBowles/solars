4d4b3ce6ab8313662854f91906bbe979c919caa7
scottBowles <shbowles@gmail.com>
2024-05-05 21:14:05 -0400

    Darken the background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,8 @@
 	div {
 		width: 100vw;
 		height: 100vh;
-		background: rgb(13, 19, 32);
-		background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%);
+		/* background: rgb(13, 19, 32);
+		background: linear-gradient(180deg, rgba(13, 19, 32, 1) 0%, rgba(8, 12, 21, 1) 100%); */
+		background: linear-gradient(180deg, rgba(6, 13, 21, 1) 0%, rgba(5, 8, 12, 1) 100%);
 	}
 </style>
