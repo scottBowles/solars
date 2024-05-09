c5df23acc40c9357a7840bf2927b625ebfbfd65c
scottBowles <shbowles@gmail.com>
2024-05-08 21:02:42 -0400

    Mobile debugging

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  ssr: {
-    noExternal: ['three']
-  }
-})
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['three']
+	},
+	server: {
+		host: true, // Open to local network and display URL
+		open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+	}
+});
