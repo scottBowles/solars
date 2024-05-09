263253827e0e8ce2870fd9719ad40d69766f713c
scottBowles <shbowles@gmail.com>
2024-05-08 21:03:18 -0400

    Comment mobile debugging

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	ssr: {
 		noExternal: ['three']
-	},
-	server: {
-		host: true, // Open to local network and display URL
-		open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
 	}
+	// server: {
+	// 	host: true, // Open to local network and display URL
+	// 	open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+	// }
 });
